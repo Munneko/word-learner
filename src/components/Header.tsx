2769b0f3631231eb89e7229379f62471a9edc986
Diana <munneko13@gmail.com>
2023-12-10 19:10:11 +0200

    header is done. started making blocks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import '../assets/styles/main.css';
-import Logo from '../assets/img/logo.svg'
+import Logo from '../assets/img/logo.svg';
+import User from '../assets/img/profile.svg';
+import Globe from '../assets/img/globe.svg';
 
 export const Header = () => {
   return (
-    <div className='Header'>
-      <div >
-      <svg width="150" height="150" >
+    <div className="header">
+      <div>
+        <svg width="150" height="150">
           <image href={Logo} width="150" height="150" />
         </svg>
-       
       </div>
       <h1>WORD LEARNER</h1>
+      <div className='header-right-icons'>
+        <div className='globe-icon'>
+          <svg width="50" height="50">
+            <image href={Globe} width="50" height="50" />
+          </svg>
+        </div>
+        <div className='user-icon'>
+          <svg width="50" height="50">
+            <image href={User} width="50" height="50" />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import '../assets/styles/main.css'
-import { Header } from '../components/Header'
+import React from 'react';
+import '../assets/styles/main.css';
+import Block from '../components/Block';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <div className="home">
+      <div className="home-greeting">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel
+        sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam
+        eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto
+        voluptate aliquam nihil, eveniet aliquid culpa officia aut!
+      </div>
+      <Block/>
+    </div>
+  );
+};
+export default Home;
